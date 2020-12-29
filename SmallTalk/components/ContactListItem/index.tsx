7bdbdfbb32ec38f233fc65f6b45b89c5842a49e5
PhatDinh <dinhphat611@gmail.com>
2020-12-29 21:26:39 +0700

    Fix

--- SmallTalk/components/ContactListItem/index.tsx
+++ SmallTalk/components/ContactListItem/index.tsx
@@ -20,59 +20,59 @@ const ContactListItem=(props: ContactListItemProps)=>{
     const navigation=useNavigation();
 
     const onClick=() =>{
-        try {
-
-            //  1. Create a new Chat Room
-            const newChatRoomData = await API.graphql(
-                graphqlOperation(
-                    createChatRoom, {
-                        input: {
-                            lastMessageID: "zz753fca-e8c3-473b-8e85-b14196e84e16"
-                        }
-                    }
-                )
-            )
-
-            if (!newChatRoomData.data) {
-                console.log(" Failed to create a chat room");
-                return;
-            }
-
-            const newChatRoom = newChatRoomData.data.createChatRoom;
-
-            // 2. Add `user` to the Chat Room
-            await API.graphql(
-                graphqlOperation(
-                    createChatRoomUser, {
-                        input: {
-                            userID: user.id,
-                            chatRoomID: newChatRoom.id,
-                        }
-                    }
-                )
-            )
-
-            //  3. Add authenticated user to the Chat Room
-            const userInfo = await Auth.currentAuthenticatedUser();
-            await API.graphql(
-                graphqlOperation(
-                    createChatRoomUser, {
-                        input: {
-                            userID: userInfo.attributes.sub,
-                            chatRoomID: newChatRoom.id,
-                        }
-                    }
-                )
-            )
-
-            navigation.navigate('ChatRoom', {
-                id: newChatRoom.id,
-                name: "Hardcoded name",
-            })
-
-        } catch (e) {
-            console.log(e);
-        }
+        // try {
+        //
+        //     //  1. Create a new Chat Room
+        //     const newChatRoomData = await API.graphql(
+        //         graphqlOperation(
+        //             createChatRoom, {
+        //                 input: {
+        //                     lastMessageID: "zz753fca-e8c3-473b-8e85-b14196e84e16"
+        //                 }
+        //             }
+        //         )
+        //     )
+        //
+        //     if (!newChatRoomData.data) {
+        //         console.log(" Failed to create a chat room");
+        //         return;
+        //     }
+        //
+        //     const newChatRoom = newChatRoomData.data.createChatRoom;
+        //
+        //     // 2. Add `user` to the Chat Room
+        //     await API.graphql(
+        //         graphqlOperation(
+        //             createChatRoomUser, {
+        //                 input: {
+        //                     userID: user.id,
+        //                     chatRoomID: newChatRoom.id,
+        //                 }
+        //             }
+        //         )
+        //     )
+        //
+        //     //  3. Add authenticated user to the Chat Room
+        //     const userInfo = await Auth.currentAuthenticatedUser();
+        //     await API.graphql(
+        //         graphqlOperation(
+        //             createChatRoomUser, {
+        //                 input: {
+        //                     userID: userInfo.attributes.sub,
+        //                     chatRoomID: newChatRoom.id,
+        //                 }
+        //             }
+        //         )
+        //     )
+        //
+        //     navigation.navigate('ChatRoom', {
+        //         id: newChatRoom.id,
+        //         name: "Hardcoded name",
+        //     })
+        //
+        // } catch (e) {
+        //     console.log(e);
+        // }
     }
     return (
         <TouchableWithoutFeedback onPress={onClick}>
